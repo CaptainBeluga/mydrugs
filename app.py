b029960f9862f77157599e170a41c0991cace548
CaptainBeluga <simone.pata27@gmail.com>
2024-04-25 14:37:32 +0200

    Custom JWT EXPIRE TIME

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def token_gen(userid,username):
     token = jwt.encode({
     "user" : username,
     "userid" : userid,
-    "exp" : datetime.utcnow() + timedelta(seconds=15)
+    "exp" : datetime.utcnow() + timedelta(minutes=50)
     },
     ENV["JWT_KEY"],algorithm='HS256')
 
